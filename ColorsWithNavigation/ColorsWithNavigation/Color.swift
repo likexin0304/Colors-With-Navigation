1a556f7ae1e983b3e17d25862f4cec8fbf7804fa
likexin <danielleeprince@gmail.com>
2017-07-19 13:41:47 +0800

    Finished the Assignment

---
+++ ColorsWithNavigation/ColorsWithNavigation/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  ColorsWithNavigation
+//
+//  Created by LIKEXIN on 7/19/17.
+//  Copyright © 2017 LIKEXIN. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
